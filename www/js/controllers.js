5b2e6bb879d90eec03c4e4a5a942809ea885ff94
benwingz <benjamin.roullet@gmail.com>
2016-04-23 16:23:17 +0200

    Add Authentification api service

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -19,6 +19,12 @@ angular.module('starter.controllers', [])
     $scope.modal = modal;
   });
 
+  $ionicModal.fromTemplateUrl('templates/signup.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modalSign = modal;
+  });
+
   // Triggered in the login modal to close it
   $scope.closeLogin = function() {
     $scope.modal.hide();
@@ -29,20 +35,45 @@ angular.module('starter.controllers', [])
     $scope.modal.show();
   };
 
+  $scope.signup = function(){
+    $scope.modalSign.show();
+  }
+
+  $scope.closeSignup = function() {
+    $scope.modalSign.hide();
+  };
+
   // Open the login modal
   $scope.logout = function() {
     $scope.loginData = {};
   };
+
+  $scope.doSignUp = function(){
+    $careApi.signup($scope.loginData.username,$scope.loginData.password).then(
+      function(success){
+        console.log("Signup success:",success);
+        $scope.loginData.id = success.user_id;
+        $scope.modalSign.hide();
+      },
+      function(error){
+        console.log("Signup error:",error);
+        $scope.loginData = {};
+      }
+    );
+  }
 
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
     console.log('Doing login', $scope.loginData);
     $careApi.login($scope.loginData.username,$scope.loginData.password).then(
       function(success){
-        console.log("Signup success:",success);
+        console.log("login success:",success);
+        $scope.loginData.id = success.user_id;
+        $scope.modal.hide();
       },
       function(error){
-        console.log("Signup error:",error);
+        console.log("login error:",error);
+        $scope.loginData = {};
       }
     );
     // Simulate a login delay. Remove this and replace with your login
